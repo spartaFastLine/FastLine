a65e6f9dc95227c8bc275ecdad02a156486eede1
seolbin01 <se01bin@suwon.ac.kr>
2025-11-05 15:06:30 +0900

    fix: CustomException 메시지 전달 누락 수정

--- common/src/main/java/com/fastline/common/exception/CustomException.java
+++ common/src/main/java/com/fastline/common/exception/CustomException.java
@@ -1,10 +1,13 @@
 package com.fastline.common.exception;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class CustomException extends RuntimeException {
 	private final ErrorCode errorCode;
+
+	public CustomException(ErrorCode errorCode) {
+		super(errorCode.getMessage());
+		this.errorCode = errorCode;
+	}
 }
