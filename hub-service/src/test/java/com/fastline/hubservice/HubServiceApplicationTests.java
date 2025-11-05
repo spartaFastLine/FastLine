e46eade3df7f8ea5bcab2c7a17a88708c4131933
jamie <northwing@naver.com>
2025-11-05 15:46:59 +0900

    feat-hub-11

---
+++ hub-service/src/test/java/com/fastline/hubservice/HubServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.fastline.hubservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = HubServiceApplication.class)
+class HubServiceApplicationTests {
+
+	@Test
+	void contextLoads() {}
+}
