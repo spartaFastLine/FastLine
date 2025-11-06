5a15939f7ca6528739bbb63f0a8cc117c357df7c
wlsql852@naver.com <wlsql852@naver.com>
2025-11-07 01:23:34 +0900

    fix - #1 - @EnableMethodSecurity를 메인 애플리케이션에서 WebSecurityConfig로 이동

--- auth-service/src/main/java/com/fastline/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/fastline/authservice/AuthServiceApplication.java
@@ -3,11 +3,9 @@ package com.fastline.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication(scanBasePackages = "com.fastline")
 @EnableJpaAuditing
-@EnableMethodSecurity(prePostEnabled = true)
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
