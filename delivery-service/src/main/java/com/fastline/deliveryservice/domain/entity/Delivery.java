2d44e904f57bc60e125fe74cc61d2ade017430ac
seolbin01 <se01bin@suwon.ac.kr>
2025-11-06 17:20:46 +0900

    feat-#16-배송, 배송 경로 기록 엔티티 추가

---
+++ delivery-service/src/main/java/com/fastline/deliveryservice/domain/entity/Delivery.java
@@ -0,0 +1,113 @@
+package com.fastline.deliveryservice.domain.entity;
+
+import com.fastline.common.jpa.TimeBaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "p_deliveries")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Delivery extends TimeBaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(name = "delivery_id")
+	private UUID deliveryId;
+
+    @Column(name = "order_id", nullable = false)
+    private UUID orderId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private DeliveryStatus status;
+
+    @Column(name = "vendor_sender_id", nullable = false)
+    private UUID vendorSenderId;
+
+    @Column(name = "vendor_receiver_id", nullable = false)
+    private UUID vendorReceiverId;
+
+    @Column(name = "start_hub_id", nullable = false)
+    private UUID startHubId;
+
+    @Column(name = "end_hub_id", nullable = false)
+    private UUID endHubId;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "recipient_username", nullable = false)
+    private String recipientUsername;
+
+    @Column(name = "recipient_slack_id", nullable = false)
+    private String recipientSlackId;
+
+    @Column(name = "vendor_delivery_manager_id", nullable = false)
+    private Long vendorDeliveryManagerId;
+
+    @OneToMany(mappedBy = "delivery", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<DeliveryPath> paths = new ArrayList<>();
+
+    public static Delivery create(
+            UUID orderId,
+            UUID vendorSenderId,
+            UUID vendorReceiverId,
+            UUID startHubId,
+            UUID endHubId,
+            String address,
+            String recipientUsername,
+            String recipientSlackId,
+            Long vendorDeliveryManagerId,
+            List<DeliveryPath> paths) {
+
+        validateDeliveryPaths(paths);
+
+        Delivery delivery = new Delivery();
+        delivery.status = DeliveryStatus.DELIVERY_PENDING;
+        delivery.orderId = orderId;
+        delivery.vendorSenderId = vendorSenderId;
+        delivery.vendorReceiverId = vendorReceiverId;
+        delivery.startHubId = startHubId;
+        delivery.endHubId = endHubId;
+        delivery.address = address;
+        delivery.recipientUsername = recipientUsername;
+        delivery.recipientSlackId = recipientSlackId;
+        delivery.vendorDeliveryManagerId = vendorDeliveryManagerId;
+
+        for (DeliveryPath path : paths) {
+            delivery.addPath(path);
+        }
+        
+        return delivery;
+    }
+
+    public void addPath(DeliveryPath path) {
+        this.paths.add(path);
+        path.setDelivery(this);
+    }
+
+    public void startDelivery() {
+        if (status != DeliveryStatus.DELIVERY_PENDING)
+            throw new IllegalStateException("대기 상태에서만 배송을 시작할 수 있습니다.");
+        status = DeliveryStatus.DELIVERY_TRANSIT;
+    }
+
+    public void completeDelivery() {
+        if (status != DeliveryStatus.DELIVERY_TRANSIT)
+            throw new IllegalStateException("배송 중 상태에서만 완료할 수 있습니다.");
+        status = DeliveryStatus.DELIVERED;
+    }
+
+    private static void validateDeliveryPaths(List<DeliveryPath> paths) {
+        if (paths == null || paths.isEmpty()) {
+            throw new IllegalArgumentException("배달 경로는 최소 1개 이상이어야 합니다");
+        }
+    }
+}
---
+++ delivery-service/src/main/java/com/fastline/deliveryservice/domain/entity/DeliveryPath.java
@@ -0,0 +1,88 @@
+package com.fastline.deliveryservice.domain.entity;
+
+import com.fastline.common.jpa.TimeBaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "p_delivery_paths")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DeliveryPath extends TimeBaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "delivery_path_id")
+    private UUID deliveryPathId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "delivery_id", nullable = false)
+    private Delivery delivery;
+
+    @Column(name = "sequence", nullable = false)
+    private int sequence;
+
+    @Column(name = "from_hub_id", nullable = false)
+    private UUID fromHubId;
+
+    @Column(name = "to_hub_id", nullable = false)
+    private UUID toHubId;
+
+    @Column(name = "exp_distance", nullable = false)
+    private int expDistance;
+
+    @Column(name = "act_distance")
+    private int actDistance;
+
+    @Column(name = "exp_duration", nullable = false)
+    private int expDuration;
+
+    @Column(name = "act_duration")
+    private int actDuration;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 30)
+    private DeliveryPathStatus status;
+
+    @Column(name = "delivery_manager_id", nullable = false)
+    private Long deliveryManagerId;
+
+    public static DeliveryPath create(
+            int sequence,
+            UUID fromHubId,
+            UUID toHubId,
+            int expDistance,
+            int expDuration,
+            Long deliveryManagerId
+    ) {
+        DeliveryPath path = new DeliveryPath();
+        path.sequence = sequence;
+        path.fromHubId = fromHubId;
+        path.toHubId = toHubId;
+        path.expDistance = expDistance;
+        path.expDuration = expDuration;
+        path.deliveryManagerId = deliveryManagerId;
+        path.status = DeliveryPathStatus.HUB_PENDING;
+        return path;
+    }
+
+    public void startTransit() {
+        if (status != DeliveryPathStatus.HUB_PENDING)
+            throw new IllegalStateException("허브 대기 상태에서만 이동 시작이 가능합니다.");
+        status = DeliveryPathStatus.HUB_TRANSIT;
+    }
+
+    public void arriveHub() {
+        if (status != DeliveryPathStatus.HUB_TRANSIT)
+            throw new IllegalStateException("이동 중 상태에서만 도착할 수 있습니다.");
+        status = DeliveryPathStatus.HUB_ARRIVED;
+    }
+
+    protected void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+    }
+}
