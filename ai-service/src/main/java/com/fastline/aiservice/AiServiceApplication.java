f10630e83d7a546c027b00c56f73fda571faa18b
minju26 <mjalswn26@gmail.com>
2025-11-07 11:47:36 +0900

    chore-#28-SecurityAutoConfiguration exclude설정

--- ai-service/src/main/java/com/fastline/aiservice/AiServiceApplication.java
+++ ai-service/src/main/java/com/fastline/aiservice/AiServiceApplication.java
@@ -2,12 +2,13 @@ package com.fastline.aiservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @EnableFeignClients
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class AiServiceApplication {
 
 	public static void main(String[] args) {
