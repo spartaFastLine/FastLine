711a6e801895614fdb05d680b97b8f9b3d09cd16
minju26 <mjalswn26@gmail.com>
2025-11-07 14:53:46 +0900

    chore-#38-Spotless 코드 스타일 적용

--- messaging-service/src/main/java/com/fastline/messagingservice/external/dto/MessageGenerationResponse.java
+++ messaging-service/src/main/java/com/fastline/messagingservice/external/dto/MessageGenerationResponse.java
@@ -1,10 +1,6 @@
 package com.fastline.messagingservice.external.dto;
 
 public record MessageGenerationResponse(
-        Boolean success,
-        String message,
-        Data data,
-        String errorCode
-) {
-    public record Data(String finalDispatchDeadline) {}
+		Boolean success, String message, Data data, String errorCode) {
+	public record Data(String finalDispatchDeadline) {}
 }
