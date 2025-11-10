130715ead6659abfc2efbbbbcbb62e6aa31038b7
minju26 <mjalswn26@gmail.com>
2025-11-10 23:03:57 +0900

    chore-#64-Spotless 코드 스타일 적용

--- common/src/main/java/com/fastline/common/auditing/BaseEntity.java
+++ common/src/main/java/com/fastline/common/auditing/BaseEntity.java
@@ -3,31 +3,30 @@ package com.fastline.common.auditing;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import java.time.Instant;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.domain.AbstractAggregateRoot;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.Instant;
-
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity<T extends BaseEntity<T>> extends AbstractAggregateRoot<T> {
 
-    @CreatedDate
-    @Column(name = "created_at", nullable = false, updatable = false)
-    private Instant createdAt;
+	@CreatedDate
+	@Column(name = "created_at", nullable = false, updatable = false)
+	private Instant createdAt;
 
-    @LastModifiedDate
-    @Column(name = "updated_at", nullable = false)
-    private Instant updatedAt;
+	@LastModifiedDate
+	@Column(name = "updated_at", nullable = false)
+	private Instant updatedAt;
 
-    @Column(name = "deleted_at")
-    private Instant deletedAt;
+	@Column(name = "deleted_at")
+	private Instant deletedAt;
 
-    public void markDeleted() {
-        this.deletedAt = Instant.now();
-    }
+	public void markDeleted() {
+		this.deletedAt = Instant.now();
+	}
 }
