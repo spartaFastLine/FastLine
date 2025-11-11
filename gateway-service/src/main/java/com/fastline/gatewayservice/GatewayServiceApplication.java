4f85c542929225e0f117cb66d63f849214f4c362
wlsql852@naver.com <wlsql852@naver.com>
2025-11-11 19:06:45 +0900

    bug - #59 - 게이트웨이에서 회원가입/로그인 인증 필요 버그 수정

--- gateway-service/src/main/java/com/fastline/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/fastline/gatewayservice/GatewayServiceApplication.java
@@ -1,14 +1,18 @@
 package com.fastline.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = "com.fastline", exclude = {
 		DataSourceAutoConfiguration.class,
-		HibernateJpaAutoConfiguration.class
+		HibernateJpaAutoConfiguration.class,
+		SecurityAutoConfiguration.class,
+		ManagementWebSecurityAutoConfiguration .class
 })
 @EnableDiscoveryClient
 public class GatewayServiceApplication {
