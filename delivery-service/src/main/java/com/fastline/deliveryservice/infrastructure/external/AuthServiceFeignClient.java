f442c391bca6c26e1d802296fbaaccdce94c5ad5
seolbin01 <se01bin@suwon.ac.kr>
2025-11-13 01:17:22 +0900

    chore-#88-내부 api 호출 엔드포인트 수정

--- delivery-service/src/main/java/com/fastline/deliveryservice/infrastructure/external/AuthServiceFeignClient.java
+++ delivery-service/src/main/java/com/fastline/deliveryservice/infrastructure/external/AuthServiceFeignClient.java
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "auth-service")
 public interface AuthServiceFeignClient {
 
-	@GetMapping("/api/managers/assign/{hubId}/{managerType}")
+	@GetMapping("/api/delivery/managers/assign/{hubId}/{managerType}")
 	ApiResponse<ManagerAssignResponse> assignManager(
 			@PathVariable UUID hubId, @PathVariable String managerType);
 
-	@GetMapping("/api/deliveries/{managerId}/complete")
+	@GetMapping("/api/delivery/managers/{managerId}/complete")
 	ApiResponse<Void> deliveryComplete(@PathVariable Long managerId);
 }
